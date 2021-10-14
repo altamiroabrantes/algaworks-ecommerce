8ef046549d79d5f850f3d134efd52c619ece158e
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-10-14 14:59:47 -0300

    2.8. Abrindo e fechando uma transação

--- src/test/java/com/algawork/ecommerce/iniciandocomjpa/OpreracoesComTransacaoTest.java
+++ src/test/java/com/algawork/ecommerce/iniciandocomjpa/OpreracoesComTransacaoTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 import com.algawork.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Produto;
 
-public class OpreracoesComTransacaoTest extends EntityManagerTest{
-	
+public class OpreracoesComTransacaoTest extends EntityManagerTest {
+
 	@Test
 	public void abrirEFecharATransacao() {
-		
-		Produto produto = new Produto();
-		
+
+//		Produto produto = new Produto();
+
 		entityManager.getTransaction().begin();
-		
-		entityManager.persist(produto);
-		entityManager.merge(produto);
-		entityManager.remove(produto);
-		
+
+//		entityManager.persist(produto);
+//		entityManager.merge(produto);
+//		entityManager.remove(produto);
+
 		entityManager.getTransaction().commit();
 	}
 }
