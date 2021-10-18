179e8ae3683b9acf0020eac75f2528c9a350406b
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-10-18 14:52:58 -0300

    2.17. Conhecendo e usando Lombok

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -1,14 +1,21 @@
 package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Produto {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	private Integer id;
 	
@@ -18,54 +25,4 @@ public class Produto {
 	
 	private BigDecimal preco;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public BigDecimal getPreco() {
-		return preco;
-	}
-
-	public void setPreco(BigDecimal preco) {
-		this.preco = preco;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produto other = (Produto) obj;
-		return Objects.equals(id, other.id);
-	}
-
-	
 }
