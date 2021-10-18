99121d5e2e306402cba9293e04c6845955f8fcf9
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-10-18 13:06:58 -0300

    2.11. Atualizando objetos com o método merge

--- src/test/java/com/algawork/ecommerce/iniciandocomjpa/OpreracoesComTransacaoTest.java
+++ src/test/java/com/algawork/ecommerce/iniciandocomjpa/OpreracoesComTransacaoTest.java
@@ -11,41 +11,60 @@ import com.algaworks.ecommerce.model.Produto;
 public class OpreracoesComTransacaoTest extends EntityManagerTest {
 
 	@Test
+	public void atualizarObjeto() {
+		Produto produto = new Produto();
+
+		produto.setId(1);
+		produto.setNome("Kindle Paperwhite");
+		produto.setDescricao("Conheça o novo Kindle");
+		produto.setPreco(new BigDecimal(599));
+
+		entityManager.getTransaction().begin();
+		entityManager.merge(produto); // faz o find automaticamente e depois executa o update.
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		Assert.assertEquals("Kindle Paperwhite", produtoVerificacao.getNome());
+
+	}
+
+	@Test
 	public void removerObjeto() {
 		Produto produto = entityManager.find(Produto.class, 3);
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.remove(produto);
 		entityManager.getTransaction().commit();
-		
-		//entityManager.clear();   Não é necessário na asserção para operação de remoção.
-		
+
+		// entityManager.clear(); Não é necessário na asserção para operação de remoção.
+
 		Produto produtoVerificacao = entityManager.find(Produto.class, 3);
 		Assert.assertNull(produtoVerificacao);
-		
+
 	}
-	
+
 	@Test
 	public void inserirOPrimeiroObjeto() {
 		Produto produto = new Produto();
-		
+
 		produto.setId(2);
 		produto.setNome("Câmera Canon");
 		produto.setDescricao("A melhor definição para suas fotos.");
 		produto.setPreco(new BigDecimal(5000));
-		
+
 		entityManager.getTransaction().begin();
 		entityManager.persist(produto);
 		entityManager.getTransaction().commit();
-		
+
 		entityManager.clear();
-		
+
 		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
 		Assert.assertNotNull(produtoVerificacao);
 	}
-	
-	
-	
+
 	@Test
 	public void abrirEFecharATransacao() {
 
