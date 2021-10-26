f6d27fe7d1fe53d5542ce482105184fac4668c1f
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-10-26 16:03:59 -0300

    4.3. Exercício: mapeando relacionamentos muitos-para-um

--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
@@ -8,13 +8,15 @@ import org.junit.Test;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.ItemPedido;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.StatusPedido;
 
 public class RelacionamentosManyToOneTest extends EntityManagerTest{
 	
 	@Test
-	public void verificarRelacionamento() {
+	public void verificarRelacionamentoPedidoCliente() {
 		Cliente cliente = entityManager.find(Cliente.class, 1);
 		
 		Pedido pedido = new Pedido();
@@ -32,6 +34,38 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest{
 		
 		Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
 		Assert.assertNotNull(pedidoVerificacao.getCliente());
+		
+	}
+	
+	@Test
+	public void verificarRelacionamentoItemPedido() {
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		Pedido pedido = new Pedido();
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		pedido.setDataPedido(LocalDateTime.now());
+		pedido.setCliente(cliente);
+		pedido.setTotal(BigDecimal.TEN);
+		
+		
+		ItemPedido itemPedido = new ItemPedido();
+		itemPedido.setPrecoProduto(produto.getPreco());
+		itemPedido.setQuantidade(1);
+		itemPedido.setPedido(pedido);
+		itemPedido.setProduto(produto);
+		
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(pedido);
+		entityManager.persist(itemPedido);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+		Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+		Assert.assertNotNull(itemPedidoVerificacao.getProduto());
 		
 	}
 }
