babf689f6ca8bfffe3935b7ce44099f9a29bbf95
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-11-29 13:55:01 -0300

    6.3. Mapeando chave composta com @IdClass 6.4. Exercício: usando @IdClass

--- src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedido.java
@@ -1,43 +1,40 @@
 package com.algaworks.ecommerce.model;
 
-import java.math.BigDecimal;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@IdClass(ItemPedidoId.class)
 @Entity
 @Table(name = "item_pedido")
 public class ItemPedido {
-	
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	@ManyToOne(optional = false)
-	@JoinColumn(name= "pedido_id")
-	private Pedido pedido;
-	
-	@ManyToOne(optional = false)
-	@JoinColumn(name= "produto_id")
-	private Produto produto;
-	
-	@Column(name= "preco_produto")
-	private BigDecimal precoProduto;
-	
-	private Integer quantidade;
-	
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "pedido_id")
+    private Integer pedidoId;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Integer produtoId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "pedido_id", insertable = false, updatable = false)
+    private Pedido pedido;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "produto_id", insertable = false, updatable = false)
+    private Produto produto;
+
+    @Column(name = "preco_produto")
+    private BigDecimal precoProduto;
+
+    private Integer quantidade;
 }
--- src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/RelacionamentosManyToOneTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.ItemPedido;
+import com.algaworks.ecommerce.model.ItemPedidoId;
 import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import com.algaworks.ecommerce.model.StatusPedido;
@@ -47,25 +48,30 @@ public class RelacionamentosManyToOneTest extends EntityManagerTest{
 		pedido.setDataCriacao(LocalDateTime.now());
 		pedido.setCliente(cliente);
 		pedido.setTotal(BigDecimal.TEN);
-		
-		
-		ItemPedido itemPedido = new ItemPedido();
-		itemPedido.setPrecoProduto(produto.getPreco());
-		itemPedido.setQuantidade(1);
-		itemPedido.setPedido(pedido);
-		itemPedido.setProduto(produto);
-		
-		
-		entityManager.getTransaction().begin();
+			
 		entityManager.persist(pedido);
-		entityManager.persist(itemPedido);
-		entityManager.getTransaction().commit();
+		// Pode ser que logo ao executar o método "persist" o JPA já faça a sincronização com a base.
+        // Mas caso isso não aconteça, o flush garante a sincronização.
+        entityManager.flush();
 		
-		entityManager.clear();
-		
-		ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
-		Assert.assertNotNull(itemPedidoVerificacao.getPedido());
-		Assert.assertNotNull(itemPedidoVerificacao.getProduto());
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPedidoId(pedido.getId());
+        itemPedido.setProdutoId(produto.getId());
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.persist(itemPedido);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(
+                ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
+        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
 		
 	}
 }
