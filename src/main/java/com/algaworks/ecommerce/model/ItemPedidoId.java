29d6c70d5e54853ddf9bc8aea434ac127c285c22
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-11-29 14:21:58 -0300

    6.5. Mapeando chave composta com @EmbeddedId

--- src/main/java/com/algaworks/ecommerce/model/ItemPedidoId.java
+++ src/main/java/com/algaworks/ecommerce/model/ItemPedidoId.java
@@ -2,6 +2,9 @@ package com.algaworks.ecommerce.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,13 +16,16 @@ import lombok.Setter;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class ItemPedidoId implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
 	@EqualsAndHashCode.Include
-	private Integer pedidoId;
-	
-	@EqualsAndHashCode.Include
-	private Integer produtoId;
+    @Column(name = "pedido_id")
+    private Integer pedidoId;
+
+    @EqualsAndHashCode.Include
+    @Column(name = "produto_id")
+    private Integer produtoId;
 }
