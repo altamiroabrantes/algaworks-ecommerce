225a1b8e522efb84c2df942dee58d178ec1139fb
altamiro.abrantes@gmail.com <altamiro.abrantes@gmail.com>
2021-12-01 16:49:30 -0300

    6.8. Declarando propriedades transientes com @Transient

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/PropriedadesTransientesTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.ecommerce.mapeamentoavancado;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+
+
+
+public class PropriedadesTransientesTest extends EntityManagerTest{
+	
+	@Test
+	public void validarPrimeiroNome() {
+		
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		Assert.assertEquals("Fernando", cliente.getPrimeiroNome());
+	}
+
+}
